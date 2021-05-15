30fb9295bc737097131f5b8897fd6258e3fd01f4
Bob <wend3llsouza@gmail.com>
2021-05-15 00:14:55 -0300

    :package: ToList with Java | Class Task

---
+++ JavaSimpleTodoList/src/TodoList/Task.java
@@ -0,0 +1,19 @@
+package TodoList;
+
+public class Task {
+	private String title;
+	private String describe;
+	
+	public Task (String title, String describe) {
+		this.title = title;
+		this.describe = describe;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getDescribe() {
+		return describe;
+	}
+}

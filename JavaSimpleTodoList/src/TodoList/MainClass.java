a1f9c5d6678a502cb4bbfacdab3cb086f2f795ba
wend3ll-souza <wend3llsouza@gmail.com>
2021-05-16 13:58:28 -0300

    :video_game: Method | createTask in TskList

--- JavaSimpleTodoList/src/TodoList/MainClass.java
+++ JavaSimpleTodoList/src/TodoList/MainClass.java
@@ -2,14 +2,16 @@ package TodoList;
 
 public class MainClass {
 	public static void main(String[] args) {
-		Task task1 = new Task("Estudar Querys SQL", "Focar em filtrar dados");
-		Task task2 = new Task("Estudar como conectar mysql com Java", "Fazer projeto durante o aprendizado");
-		Task task3 = new Task("Praticar Array em java", "Editar elemento de um array");
-		TskList list = new TskList();
 		
-		list.add(task1);
-		list.add(task2);
-		list.add(task3);
+		TskList list = new TskList("Tarefas de casa:");
+		
+		list.createTask("Lavar louça", "Lave pela manhã o excedemte do dia anterior,"
+				+ " depois mantenha a louça sempre limpa se puder :)");
+		
+		list.createTask("Limpar o quintal", " Ao limpar o quintal aproveite e regue as plantas)");
+		
+		list.createTask("Dar ração ao cachorros", "Cozinhar ossos e carne para fazer caldo para ração");
+		
 		
 		list.getList();
 	}
--- JavaSimpleTodoList/src/TodoList/TskList.java
+++ JavaSimpleTodoList/src/TodoList/TskList.java
@@ -5,14 +5,21 @@ import java.util.*;
 public class TskList {
 	
 	private ArrayList<Task> list = new ArrayList<>();
+	private String name;
+	
+	public TskList(String name) {
+		this.name = name;
+	}
 	
 	public void getList() {
+		System.out.println(name);
 		 for(int key = 0; key < list.size(); key++) {
 				System.out.println(key + "| " +list.get(key) + ",");
 		}
 	}
 	
-	public void add(Task task) {
+	public void createTask(String title, String describe) {
+		Task task = new Task(title, describe);
 		list.add(task);
 	}
 	
